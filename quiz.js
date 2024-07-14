28a20deb380dde9c4091ed17f80c1bb6fb6c7646
Alhassan Abdul-Aziz <117979675+Abdul-Aziz1199@users.noreply.github.com>
2024-07-14 22:40:01 +0000

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -7,13 +7,14 @@
     let correctAnswer = "4";
 
     let  userAnswer = document.querySelector('input[name="quiz"]:checked').value ;
-    if (userAnswer === correctAnswer ){
+
+    if (userAnswer === correctAnswer){
         document.getElementById("feedback").textContent = "Correct! Well done";
     }
     else if(userAnswer ==="22"){
         document.getElementById("feedback").textContent = "Thats incorrect. Try again";
 
-    }else if(userAnswer ==="3"){
+    } else if(userAnswer ==="3"){
         document.getElementById("feedback").textContent = "Thats incorrect. Try again";
     }
     else{
