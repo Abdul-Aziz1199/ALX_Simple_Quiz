861f796a06f70ffc81f8c0763229e4663555f9be
Alhassan Abdul-Aziz <117979675+Abdul-Aziz1199@users.noreply.github.com>
2024-07-14 22:44:42 +0000

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -9,16 +9,16 @@
     let  userAnswer = document.querySelector('input[name="quiz"]:checked').value ;
 
     if (userAnswer === correctAnswer){
-        document.getElementById("feedback").textContent = "Correct! Well done";
+        document.getElementById("feedback").textContent = "Correct! Well done.";
     }
     else if(userAnswer ==="22"){
-        document.getElementById("feedback").textContent = "Thats incorrect. Try again";
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
 
     } else if(userAnswer ==="3"){
-        document.getElementById("feedback").textContent = "Thats incorrect. Try again";
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
     }
     else{
-        document.getElementById("feedback").textContent = "Thats incorrect. Try again";
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
     }
 };
 
